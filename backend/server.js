bb3de8f80bcb58fa1ceba66a03b2656c2f76aa62
viveksgonal <gonalvivek7@gmail.com>
2022-02-12 09:57:50 +0530

    first

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ app.use('/api/users',userRoutes);
 //-----deploy---
 __dirname = path.resolve()
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,"frontend/build")));
+    app.use(express.static(path.join(__dirname,"./frontend/build")));
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+        res.sendFile(path.resolve(__dirname,"./frontend/build/index.html"));
     })
 }else{
     app.get('/',(req,res) =>{
